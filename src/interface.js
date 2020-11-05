a6dd899f285e54cd71a6676452fdc348b9b75373
charlie-galb <galbraith.charlie@gmail.com>
2020-11-05 14:08:11 +0000

    Raise, lower, and reset buttons now linked to JS

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temp').text(thermostat.temp);
-})
+  $('#temp').text(thermostat.getCurrentTemp());
+
+  $('#raise-temp').click(function(){
+    thermostat.raise();
+    $('#temp').text(thermostat.getCurrentTemp());
+  });
+
+  $('#lower-temp').click(function(){
+    thermostat.lower();
+    $('#temp').text(thermostat.getCurrentTemp());
+  });
+
+  $('#reset-temp').click(function(){
+    thermostat.reset();
+    $('#temp').text(thermostat.getCurrentTemp());
+  });
+});
