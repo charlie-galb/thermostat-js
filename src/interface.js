14b883972c4b1b463ba56f7e70f6978904f50950
charlie-galb <galbraith.charlie@gmail.com>
2020-11-05 18:08:29 +0000

    Feature: App displays local weather data from city selected by user

--- src/interface.js
+++ src/interface.js
@@ -28,8 +28,26 @@ $(document).ready(function() {
     updateTemperature();
   });
 
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
   function updateTemperature() {
     $('#temp').text(thermostat.getCurrentTemp());
     $('#temp').attr('class', thermostat.energyUsage());
   }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=5e762ab9d47e15f45648245ba09e15c9';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#local-weather').text(data.main.temp);
+    })
+  }
+
 });
